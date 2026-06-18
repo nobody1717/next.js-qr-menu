586798c1ae9dc81d5e53ac095078295966e2e3e6
nobody <105120943+nobody1717@users.noreply.github.com>
2026-06-18 19:33:41 +0400

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  content: ["./app/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
-  theme: {
-    extend: {
-      fontFamily: { serif: ['Cormorant Garamond', 'serif'], sans: ['Inter', 'sans-serif'] }
-    }
-  }
-}
