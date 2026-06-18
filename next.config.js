762aeccdc6ce7fd286dff36c9280626be8e9e836
nobody <105120943+nobody1717@users.noreply.github.com>
2026-06-18 19:03:17 +0400

    Configure Next.js for standalone output

--- next.config.js
+++ next.config.js
@@ -1,1 +1,6 @@
-module.exports = {}
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone', // Попробуйте добавить эту строчку, если не поможет
+}
+
+module.exports = nextConfig
